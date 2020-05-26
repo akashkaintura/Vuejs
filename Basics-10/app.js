221352a9bfed4f62379db8e95469665ea2ed272c
akashkaintura <akashkaintura@yahoo.com>
2020-05-26 13:50:28 +0530

    for loop in vuejs

---
+++ Basics-10/app.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+        characters: ['mashida', 'yogiyo', 'goku'],
+        Actors:[
+                {name:'abc', age:23},
+                {name:'dec', age:54},
+                {name:'ken', age:38},
+            ]       
+    },
+
+    methods: {
+        
+
+    },
+    
+    computed:{
+
+    }
+});
