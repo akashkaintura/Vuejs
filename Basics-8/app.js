7cf42af4da901bf654986b8d3845bb1c5b9844ea
akashkaintura <akashkaintura@yahoo.com>
2020-05-26 13:49:04 +0530

    Dynamic CSS

---
+++ Basics-8/app.js
@@ -0,0 +1,24 @@
+new Vue({
+
+    el: '#vue-app',
+
+    data: {
+        available: true,
+        nearby: true,
+    },
+    
+    methods: {
+        
+    },
+
+    computed:{
+
+        compClasses: function(){
+            return{
+                available: this.available,
+                nearby: this.nearby
+            }
+            
+        }
+    }
+});
