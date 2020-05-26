1c96b46d98515a593a8e1e189811fcb6c80b350f
akashkaintura <akashkaintura@yahoo.com>
2020-05-26 11:28:01 +0530

    Events

---
+++ Basics-4/app.js
@@ -0,0 +1,32 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+       age: 23,
+       x: 0,
+       y: 0
+    },
+    methods: {
+       add: function(inc)
+       {
+           this.age += inc;
+       },
+       
+       subtract: function(dec)
+       {
+           this.age -= dec;
+       },
+
+       updateXY: function(event)
+       {
+       this.x = event.offsetX;
+       this.y = event.offsetY;
+       },
+
+       click: function()
+       {
+           alert('You clicked me');
+       }
+    
+ }
+    
+});
---
+++ Basics-5/app.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+        
+    },
+    methods: {
+        logName: function(){
+            console.log('Entered Name');
+        },
+        logAge: function(){
+            console.log('Entered Age');
+        }
+
+    }
+});
